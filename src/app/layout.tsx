c42b13abe71a18ca7662496710f37ab891a4e3cc
Mot0511 <suvorov.matvej9@gmail.com>
2024-06-30 11:59:48 +0300

    _

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/header";
-import Head from "next/head";
 import 'bootstrap/dist/css/bootstrap.css'
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,12 +16,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
-      <Head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"></script>    
-      </Head>
       <body className={inter.className} data-bs-theme="dark">
         <div className="container">
           <Header />
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,6 +8,7 @@ const Header = () => {
     const [isMobile, setIsMobile] = useState<boolean>(false)
 
     useEffect(() => {
+        import("bootstrap/dist/js/bootstrap");
         if (window.innerWidth <= 991){
             setIsMobile(true)
         }
@@ -20,7 +21,7 @@ const Header = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className={`collapse navbar-collapse ${!isMobile ? 'd-flex justify-content-end' : ''}`} id="navbarNav">
+                <div className={`collapse navbar-collapse${!isMobile ? ' d-flex justify-content-end' : ''}`} id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="navbar-item">
                             <Link href="/" className="nav-link fs-4">Главная</Link>
